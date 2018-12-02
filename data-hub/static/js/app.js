71eee1443fb00b072c4d0ec9b03ef6a7bdd7a5a1
Petar Stevovski <stevovski.petar@gmail.com>
2018-12-02 17:46:48 +0100

    Added option to take user input to display profiles.

--- data-hub/static/js/app.js
+++ data-hub/static/js/app.js
@@ -1,9 +1,21 @@
-// Import modules
-import {Github} from "./github.js";
-import {UI} from "./ui.js";
-
 // Instantiate classes
 const github = new Github();
 const ui = new UI();
 
+const form = document.querySelector("#form");
+const searchUser = document.querySelector("#searchUser");
 
+form.addEventListener("submit", (e) => {
+    const theUser = searchUser.value;
+
+    if(!theUser) {
+        alert("Please enter a username")
+    } else {
+        github.getUser(theUser).then(data => {
+            console.log(data.profile);
+            ui.displayData(data.profile);
+        })
+    }
+
+    e.preventDefault();
+})

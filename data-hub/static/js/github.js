c39b8e8ca4bd14084390bbf3eba4a018ec57a54b
Petar Stevovski <stevovski.petar@gmail.com>
2018-12-02 17:47:11 +0100

    Added credentials.

--- data-hub/static/js/github.js
+++ data-hub/static/js/github.js
@@ -1,5 +1,15 @@
-export class Github {
+class Github {
     constructor() {
-        this.test = "Hello this is a test."
+        this.id = "d5691091cde0221e3554";
+        this.secret = "575acf839239b4017108b0089ec83e9c987bf5ca";
+    }
+
+    async getUser(theUser) {
+        const response = await fetch(`https://api.github.com/users/${theUser}`);
+        const profile = await response.json();
+
+        return {
+            profile
+        }
     }
 }

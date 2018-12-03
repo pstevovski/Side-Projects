f631230613eb7e3f55eebcb98cd71dff993b58b4
Petar Stevovski <stevovski.petar@gmail.com>
2018-12-03 15:32:23 +0100

    Added option to asynchronously fetc profile and repo data for user.

--- data-hub/static/js/github.js
+++ data-hub/static/js/github.js
@@ -2,14 +2,23 @@ class Github {
     constructor() {
         this.id = "d5691091cde0221e3554";
         this.secret = "575acf839239b4017108b0089ec83e9c987bf5ca";
+        this.count = 5;
+        this.sort = "updated";
     }
 
     async getUser(theUser) {
-        const response = await fetch(`https://api.github.com/users/${theUser}`);
+        const response = await fetch(`https://api.github.com/users/${theUser}?client_id=${this.id}&client_secret=${this.secret}`);
+        
+        // Repos response
+        const reposResponse = await fetch(`https://api.github.com/users/${theUser}/repos?per_page=${this.count}&sort=${this.sort}&direction="asc"&client_id=${this.id}&clien_secret=${this.secret}`);
+        
+        
         const profile = await response.json();
+        const repos = await reposResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }

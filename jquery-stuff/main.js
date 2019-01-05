1ebdb21d8b176839821cdc1d11334e3896d604d4
Petar Stevovski <stevovski.petar@gmail.com>
2019-01-05 01:22:25 +0100

    Updated jQuery exercises.

--- jquery-stuff/main.js
+++ jquery-stuff/main.js
@@ -1,27 +1,27 @@
 // // Change the body background color
-// $("body").css("background-color", "#222");
+$("body").css("background-color", "#222");
 
-// // Change the title and list items color
-// $("h1").css("color", "#f5f5f5");
-// $("ul li").css("color", "#fff");
+// Change the title and list items color
+$("h1").css("color", "#f5f5f5");
+$("ul li").css("color", "#fff");
 
-// // Add mroe styling to a ceratin element at once
-// // const style = {
-// //     fontSize: "22px",
-// //     fontFamily: "Montserrat Medium",
-// //     color: "orangered",
-// //     padding: "10px"
-// // }
-
-// // $("#number--two").css(style);
-
-// // OR insert the object inline
-// $("#number--two").css({
+// Add more styling to a ceratin element at once
+// const style = {
 //     fontSize: "22px",
 //     fontFamily: "Montserrat Medium",
 //     color: "orangered",
 //     padding: "10px"
-// })
+// }
+
+// $("#number--two").css(style);
+
+// OR insert the object inline
+$("#number--two").css({
+    fontSize: "22px",
+    fontFamily: "Montserrat Medium",
+    color: "orangered",
+    padding: "10px"
+})
 
 
 /* EXERCISE 1:
@@ -44,3 +44,37 @@ $(".highlight").css("width", "200px");
 $("#third").css("border", "2px solid orange");
 
 $("div:first-of-type").css("color", "gold");
+
+// CLICK EVENTS
+$("button").click(function() {
+    $("body").css("background", "orangered");
+})
+
+// Supports ES6 syntax
+$("#btnTwo").click(function() {
+    $(this).css("background", "blue");
+    $("body").css("background", "#222");
+})
+
+// KEYDOWN/UP/PRESS EVENTS
+// $("input").keypress(function() {
+//     const changed = $(this).val();
+//     $("h1").text(changed);
+// })
+
+$("input").keyup(function() {
+    const changed = $(this).val();
+    $("h1").text(changed);
+});
+
+// $("input").keydown(function() {
+//     const changed = $(this).val();
+//     $("h1").text(changed);
+// });
+
+// FADE & SLIDE events
+$("#btnTwo").on('click', ()=>{
+    $("h1").fadeOut(1000, ()=>{
+        $("div").slideUp(500);
+    });
+})
